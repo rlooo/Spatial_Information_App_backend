3ee1b0bf5e13d3d9f31ef4e9240e33b91ca4e777
jey0623 <jey0623@naver.com>
2022-06-18 21:43:51 +0900

    이미지 업로드

--- board/admin.py
+++ board/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from board.models import PutOut, LookFor, ApplySpace, BldRgstService, QnA, Notice, PutOutImage
+from board.models import PutOut, LookFor, ApplySpace, BldRgstService, QnA, Notice, BuildingImage
 
 admin.site.register(PutOut)
 admin.site.register(LookFor)
@@ -9,4 +9,4 @@ admin.site.register(ApplySpace)
 admin.site.register(BldRgstService)
 admin.site.register(QnA)
 admin.site.register(Notice)
-admin.site.register(PutOutImage)
+admin.site.register(BuildingImage)
